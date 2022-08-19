a347b650c3ffb00991608270941d847da49444f7
RodrigoLima <rodrigolima@nasajon.com.br>
2022-08-19 17:38:38 -0300

    016 - Adicionados os primeiros arquivos de autenticação de login

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:projeto_controle_financeiro/core/app_module.dart';
 import 'package:projeto_controle_financeiro/core/app_widget.dart';
+import 'package:projeto_controle_financeiro/core/auth/services/auth_service.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(ModularApp(
-    module: AppModule(),
-    child: const AppWidget(),
-  ));
+void main() async {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => AuthService()),
+      ],
+      child: ModularApp(
+        module: AppModule(),
+        child: const AppWidget(),
+      ),
+    ),
+  );
 }
